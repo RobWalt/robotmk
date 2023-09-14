45d9585f3ec13d165ed583bbc1c835a58d8f5c05
Simon Meggle <simon.meggle@tribe29.com>
2023-09-14 17:04:13 +0200

    Also install vscode, git, sysinternals with choco; do not force installation

--- vagrant/scripts/provision.ps1
+++ vagrant/scripts/provision.ps1
@@ -1,7 +1,10 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y --force googlechrome
-choco install -y --force python
-choco install -y --force nushell
+choco install -y googlechrome
+choco install -y python
+choco install -y nushell
+choco install -y sysinternals
+choco install -y vscode
+choco install -y git.install
 
 (new-object net.webclient).DownloadFile("https://nightly.link/elabit/robotmk/actions/artifacts/878920132.zip", "C:\Users\vagrant\Downloads\rcc.zip")
 Expand-Archive "C:\Users\vagrant\Downloads\rcc.zip" -Force -DestinationPath "C:\Users\vagrant\Downloads\"
